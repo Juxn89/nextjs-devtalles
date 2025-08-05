0d48d2ff63dfe18c26293ffadf146873a3383210
Juan Gómez <gb.jc@outlook.com>
2025-08-05 17:17:41 -0400

    Added routes samples

---
+++ app/pricing/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PricingPage = () => {
+	return (
+		<div>
+			<h1>Pricing</h1>
+			<p>We offer competitive pricing for our services.</p>
+		</div>
+	)
+}
+
+export default PricingPage
