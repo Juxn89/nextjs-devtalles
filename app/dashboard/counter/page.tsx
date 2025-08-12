fccdb15d5d0f56119f538bfe4d4362b9487ae157
Juan Gómez <gb.jc@outlook.com>
2025-08-12 15:29:42 -0400

    Counter page with server and client components.

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Metadata } from "next"
+import { CartCounter } from "@/app/shopping-cart/components/CartCounter"
+
+export const metadata: Metadata = {
+	title: "Shopping Cart",
+	description: "A simple counter"
+}
 
 const CounterPage = () => {
+
 	return (
-		<div>Counter page</div>
+		<div className='flex flex-col items-center justify-center w-full h-full'>
+			<span>Products in cart</span>
+			<CartCounter value={10} />
+		</div>
 	)
 }
 
