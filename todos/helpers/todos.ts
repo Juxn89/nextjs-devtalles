770b4c125ee22d5cb0eea925ecd9c24f110d7d08
Juan Gómez <gb.jc@outlook.com>
2025-08-25 21:28:48 +0000

    Added handler for updating  ToDo.

---
+++ todos/helpers/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "@prisma/client";
+
+export const updateTodo = async (id: string, completed: boolean): Promise<Todo> => {
+	const body = { completed }
+
+	const todo = await fetch(`/api/todos/${ id }`, {
+		method: 'PUT',
+		body: JSON.stringify(body),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+	.then(response => response.json())
+
+	console.log(todo)
+
+	return todo
+
+}
