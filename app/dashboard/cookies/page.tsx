2caa6cdebcc413383b66c84453b8167b439201eb
Juan Gómez <gb.jc@outlook.com>
2025-08-27 20:14:16 +0000

    Handle cookies in server side.

--- app/dashboard/cookies/page.tsx
+++ app/dashboard/cookies/page.tsx
@@ -1,17 +1,21 @@
-import { TabBar } from "@/app/components/TabBar";
 import { Metadata } from "next";
+import { cookies } from "next/headers";
+import { TabBar } from "@/app/components/TabBar";
 
 export const metadata: Metadata = {
 	title: "Cookies",
 	description: "Manage your cookies"
 }
 
-export default function CookiePage() {
+export default async function CookiePage() {
+	const cookieStore = await cookies();
+	const cookieTab = cookieStore.get("selectedTab")?.value ?? '1';
+
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
 			<div className="flex flex-col">
 				<span className="text-3xl">Tabs</span>
-				<TabBar />
+				<TabBar currentTab={ +cookieTab } />
 			</div>
 		</div>
 	);
