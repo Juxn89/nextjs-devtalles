213375ac51a21c5fa014b0267bf0ca15b83ac97b
Juan Gómez <gb.jc@outlook.com>
2025-09-08 20:54:27 +0000

    Added Sidebar.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { PageNotFound } from "./ui/not-found/NotFound";
 export { ProductGrid } from "./products/product-grid/ProductGrid";
 export { ProductGridItem } from "./products/product-grid/ProductGridItem";
+export { Sidebar } from "./ui/sidebar/Sidebar";
 export { Title } from "./ui/title/Title";
 export { TopMenu } from "./ui/top-menu/TopMenu";
